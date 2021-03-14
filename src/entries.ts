231d8da19c1ac1fd2460ef4a883a2440e52cac57
kqito <29191111+kqito@users.noreply.github.com>
2021-03-14 11:16:40 +0900

    Fix entries's key

--- src/entries.ts
+++ src/entries.ts
@@ -1,8 +1,9 @@
-export type Entries<T> = T extends Record<string, any>
+export type Entries<T> = T extends Record<PropertyKey, any>
   ? {
       [P in keyof T]: [P, T[P]];
     }[keyof T][]
   : never;
 
-export const entries = <T extends Record<string, any>>(values: T): Entries<T> =>
-  Object.entries(values) as Entries<T>;
+export const entries = <T extends Record<PropertyKey, any>>(
+  values: T
+): Entries<T> => Object.entries(values) as Entries<T>;
